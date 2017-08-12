96c27f6b6a5ff84022312327fb81596bc074c85c
Sean Heelan <sean@vertex.re>
2017-08-12 21:08:27 +0100

    Added get + build scripts

---
+++ build.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+ORIGIN=`pwd`
+AFL_DIR="$ORIGIN/downloads/afl-2.49b"
+PHP_DIR="$ORIGIN/downloads/php-src"
+PHP_INSTALL_DIR="$PHP_DIR/install"
+
+set -e
+function ret_to_origin {
+	cd "$ORIGIN"
+}
+trap ret_to_origin EXIT
+
+if [[ ! -d "$AFL_DIR" || ! -d "$PHP_DIR" ]]; then
+	echo "[!] AFL or PHP are missing. Run ./get.sh first."
+	exit 1
+fi
+
+# AFL
+function build_afl_clang_fast {
+	if hash clang 2>/dev/null; then
+		cd "$AFL_DIR/llvm_mode"
+		make
+	else
+		echo -n "[!] clang is not installed. "
+		echo "afl-clang-fast will not be built."
+	fi
+}
+
+if [[ -f "$AFL_DIR/afl-fuzz" ]]; then
+	if [[ -f "$AFL_DIR/afl-clang-fast" ]]; then 
+		echo "[+] AFL already built. Skipping ..."
+	else
+		echo "[+] Building afl-clang-fast"
+		build_afl_clang_fast
+	fi
+	cd "$ORIGIN"
+else
+	echo "[+] Building AFL ..."
+	cd "$AFL_DIR"
+	make 
+	build_afl_clang_fast
+	cd "$ORIGIN"
+fi
+
+if [[ -f "$AFL_DIR/afl-clang-fast" ]]; then
+	AFL_CC="$AFL_DIR/afl-clang-fast"
+else
+	AFL_CC="$AFL_DIR/afl-gcc"
+fi
+
+if [[ ! -f "$AFL_CC" ]]; then
+	echo "[!] Something has gone wrong. $AFL_CC does not exist."
+	exit 1;
+fi
+
+# PHP
+
+if [[ -f "$PHP_INSTALL_DIR/bin/php" ]]; then
+	echo "[+] PHP already built. Skipping ..."
+	exit 1
+fi
+
+echo "[+] Building PHP with $AFL_CC ..."
+cd "$PHP_DIR"
+./buildconf --force
+./configure CC="$AFL_CC" --prefix="$PHP_INSTALL_DIR"
+make -j `grep -c ^processor /proc/cpuinfo`
+make install
+
+cd "$ORIGIN"
+
+echo "[+] Done"
---
+++ get.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+DOWNLOADS_DIR=`pwd`/downloads
+PHP_DST="$DOWNLOADS_DIR/php-src"
+AFL_DST="$DOWNLOADS_DIR/afl-2.49b"
+PHP_GIT="https://github.com/php/php-src.git"
+AFL_VER="2.49b"
+AFL_TGZ="afl-2.49b.tgz"
+AFL_URL="http://lcamtuf.coredump.cx/afl/releases/$AFL_TGZ"
+
+set -e
+
+if [[ ! -d "$DOWNLOADS_DIR" ]]; then
+	echo "[+] Creating downloads directory ..."
+	mkdir "$DOWNLOADS_DIR"
+fi
+
+# PHP
+if [[ -d "$PHP_DST" ]]; then 
+	echo "[+] PHP's source already exists. Skipping ..."
+else 
+	echo "[+] Getting PHP ..."
+ 	git clone "$PHP_GIT" "$PHP_DST"
+fi
+
+# AFL
+if [[ -d "$AFL_DST" ]]; then
+	echo "[+] AFL ($AFL_VER) already exists. Skipping ..."
+else
+	echo "[+] Getting AFL ($AFL_VER) ..."
+	wget --directory-prefix="$DOWNLOADS_DIR" "$AFL_URL"
+	tar xf "$DOWNLOADS_DIR/$AFL_TGZ" -C "$DOWNLOADS_DIR"
+	rm "$DOWNLOADS_DIR/$AFL_TGZ"
+fi
+
+echo "[+] Done"
