1f371c75315fd92667a6cbae47d69e90a5a349ed
Sean Heelan <sean@vertex.re>
2017-08-13 17:39:00 +0100

    Added gc_collect_cycles() to driver

--- aux/driver.php
+++ aux/driver.php
@@ -2,9 +2,10 @@
 $handle = fopen($argv[1], "r");
 if ($handle) {
     $line = fgets($handle);
-    #echo $line;
+    echo $line;
     fclose($handle);
     $data = unserialize($line);
+    gc_collect_cycles();
     var_dump($data);
 } else {
     echo "Failed to open " + $argv[1];
